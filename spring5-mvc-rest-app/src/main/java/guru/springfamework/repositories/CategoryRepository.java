f4b686e4ef2563573a050fba5c25becfee9fe415
yerasoni20 <yerasoni20@gmail.com>
2022-04-13 13:15:45 -0400

    splitting into modules

--- spring5-mvc-rest-app/src/main/java/guru/springfamework/repositories/CategoryRepository.java
+++ spring5-mvc-rest-app/src/main/java/guru/springfamework/repositories/CategoryRepository.java
@@ -1,10 +1,6 @@
 package guru.springfamework.repositories;
 
 import guru.springfamework.domain.Category;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
