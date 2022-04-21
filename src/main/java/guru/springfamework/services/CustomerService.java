19308713410ad885e6537cfb486d2dcc11e10e60
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-04-21 08:32:04 -0400

    added delete customer functionality

--- src/main/java/guru/springfamework/services/CustomerService.java
+++ src/main/java/guru/springfamework/services/CustomerService.java
@@ -4,9 +4,7 @@ import guru.springfamework.api.v1.model.CustomerDTO;
 
 import java.util.List;
 
-/**
- * Created by jt on 9/27/17.
- */
+
 public interface CustomerService {
 
     List<CustomerDTO> getAllCustomers();
