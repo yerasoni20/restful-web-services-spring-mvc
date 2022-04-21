efdb7f8e17cc52851e4441dfd61f3906378761e2
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-04-21 08:23:03 -0400

    added customer url as json property

--- src/test/java/guru/springfamework/controllers/v1/AbstractRestControllerTest.java
+++ src/test/java/guru/springfamework/controllers/v1/AbstractRestControllerTest.java
@@ -2,9 +2,6 @@ package guru.springfamework.controllers.v1;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-/**
- * Created by jt on 9/28/17.
- */
 public abstract class AbstractRestControllerTest {
 
     public static String asJsonString(final Object obj) {
