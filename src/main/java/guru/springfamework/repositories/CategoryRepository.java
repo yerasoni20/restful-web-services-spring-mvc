a6b9bcb78adfbd2d26b554134a2a43c5373fdc66
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-04-21 09:05:47 -0400

    Added Category Service

--- src/main/java/guru/springfamework/repositories/CategoryRepository.java
+++ src/main/java/guru/springfamework/repositories/CategoryRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by jt on 9/24/17.
- */
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String name);
 }
