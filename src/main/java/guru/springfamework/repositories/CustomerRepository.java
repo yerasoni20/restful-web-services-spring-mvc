6159560044e2fbe8224fa57acb5da7d4136dbc35
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-04-22 06:42:12 -0400

    Added customer mapping, domain, service, repository, controller and test cases

--- src/main/java/guru/springfamework/repositories/CustomerRepository.java
+++ src/main/java/guru/springfamework/repositories/CustomerRepository.java
@@ -3,8 +3,6 @@ package guru.springfamework.repositories;
 import guru.springfamework.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by jt on 9/27/17.
- */
+
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 }
